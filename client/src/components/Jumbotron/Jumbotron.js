516f07d2f433c8cad18a1d232b6b1b9c9411a454
jyanglau88 <jyanglau88@gmail.com>
2018-11-05 18:16:53 -0800

    new file

--- client/src/components/Jumbotron/Jumbotron.js
+++ client/src/components/Jumbotron/Jumbotron.js
@@ -3,7 +3,7 @@ import React from "react";
 const Jumbotron = ({ children }) => (
   <div
     style={{ height: 150, clear: "both", paddingTop: 120, textAlign: "center" }}
-    className="jumbotron jumbotron-fluid"
+    className="jumbotron"
   >
     {children}
   </div>
